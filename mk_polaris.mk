73045e59bca8fa75aaa0be7c8b4bb25a259f93b1
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    polaris: Consider mk_polaris product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- mk_polaris.mk
+++ mk_polaris.mk
@@ -21,9 +21,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := full_polaris
+PRODUCT_NAME := mk_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="polaris-user 8.0.0 OPR1.170623.032 V9.5.18.0.ODGCNFA release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.18.0.ODGCNFA:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=wood
