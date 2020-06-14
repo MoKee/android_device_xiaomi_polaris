26d8397904bfa533baf486b8a7244e2f9250b2bc
Martincz Gao <martincz@mokeedev.com>
2020-06-14 21:14:59 +0800

    polaris: mokee-ify

--- device.mk
+++ device.mk
@@ -19,10 +19,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-mokee/mokee-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
